dc07bcd16d52b3c6bc5f0e7b1992dcd9baeded0a
abhijeetanandshah <abhijeet.anand99@gmail.com>
2019-02-05 18:56:49 +0530

    Abhijeet | First Commit

---
+++ WebApi/WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json.Serialization;
+
+
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //Enable CORS
+            config.EnableCors(new EnableCorsAttribute("http://localhost:4200", headers: "*", methods: "*"));
+
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Set JSON formatter as default one and remove XmlFormatter
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+        }
+    }
+}
---
+++ WebApi/WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private DBModel db = new DBModel();
+        
+        // GET: api/Employee
+        public IQueryable<newTblEmployee> GetnewTblEmployees()
+        {
+            return db.newTblEmployees;
+        }
+
+        ////GET: api/Employee/5
+        //[ResponseType(typeof(newTblEmployee))]
+        //public IHttpActionResult GetnewTblEmployee(int id)
+        //{
+        //    newTblEmployee newTblEmployee = db.newTblEmployees.Find(id);
+        //    if (newTblEmployee == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(newTblEmployee);
+        //}
+
+        // PUT: api/Employee/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutnewTblEmployee(int id, newTblEmployee newTblEmployee)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            if (id != newTblEmployee.EmployeeID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(newTblEmployee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!newTblEmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.OK);
+        }
+
+        // POST: api/Employee
+        [ResponseType(typeof(newTblEmployee))]
+        public IHttpActionResult PostnewTblEmployee(newTblEmployee newTblEmployee)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            db.newTblEmployees.Add(newTblEmployee);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = newTblEmployee.EmployeeID }, newTblEmployee);
+        }
+
+        // DELETE: api/Employee/5
+        [ResponseType(typeof(newTblEmployee))]
+        public IHttpActionResult DeletenewTblEmployee(int id)
+        {
+            newTblEmployee newTblEmployee = db.newTblEmployees.Find(id);
+            if (newTblEmployee == null)
+            {
+                return NotFound();
+            }
+
+            db.newTblEmployees.Remove(newTblEmployee);
+            db.SaveChanges();
+
+            return Ok(newTblEmployee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool newTblEmployeeExists(int id)
+        {
+            return db.newTblEmployees.Count(e => e.EmployeeID == id) > 0;
+        }
+    }
+}
